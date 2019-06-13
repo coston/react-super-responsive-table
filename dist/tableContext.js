cddefa109427f5a5da407ba1d1d5eb37ed8708db
Coston Perkins <costonperkins@me.com>
2019-06-13 13:26:44 -0500

    Remove createContext ponyfill (breaking < 16)

--- dist/tableContext.js
+++ dist/tableContext.js
@@ -7,16 +7,11 @@ exports.Consumer = exports.Provider = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
-var _createReactContext = _interopRequireDefault(require("create-react-context"));
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
-// use the polyfill only if Context API not available (react<16)
-var createContext = _react["default"].createContext || _createReactContext["default"];
-
-var _createContext = createContext({}),
-    Provider = _createContext.Provider,
-    Consumer = _createContext.Consumer;
+var _React$createContext = _react["default"].createContext({}),
+    Provider = _React$createContext.Provider,
+    Consumer = _React$createContext.Consumer;
 
 exports.Consumer = Consumer;
 exports.Provider = Provider;
