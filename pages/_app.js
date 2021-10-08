e9023ff9b53f5d3bba4f4ab86f128ba9745d2eba
Coston <costonperkins@me.com>
2021-10-08 15:29:28 +0000

    fix: demo build

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import './styles.css';
+import '../src/SuperResponsiveTableStyle.css';
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
