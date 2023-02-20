44eed94c40375fc64bccfa0d8c45f43cd404d28d
Coston Perkins <costonperkins@me.com>
2023-02-20 15:22:33 -0600

    docs: update demo code

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ class App extends React.Component {
           Th,
           Td,
         }}
-        code={headerCode}
+        code={tableCode}
       />
     );
   }
@@ -41,49 +41,47 @@ npm install react-super-responsive-table --save
 ## Usage
 
 1. import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table'
-2. Copy or import ~react-super-responsive-table/dist/SuperResponsiveTableStyle.css~ into your project
+2. Copy or import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css' into your project
 3. Write your html table with the imported components.
 `;
 
-const headerCode = ` () => (
-  <Table>
-    <Thead>
-      <Tr>
-        <Th>Event</Th>
-        <Th>Date</Th>
-        <Th>Location</Th>
-        <Th>Organizer</Th>
-        <Th>Theme</Th>
-        <Th>Agent</Th>
-      </Tr>
-    </Thead>
-    <Tbody>
-      <Tr>
-        <Td>Tablescon</Td>
-        <Td>9 April 2019</Td>
-        <Td>East Annex</Td>
-        <Td>Super Friends</Td>
-        <Td>Data Tables</Td>
-        <Td>Coston Perkins</Td>
-      </Tr>
-      <Tr>
-        <Td>Capstone Data</Td>
-        <Td>19 May 2019</Td>
-        <Td>205 Gorgas</Td>
-        <Td>Data Underground</Td>
-        <Td>Data Scence</Td>
-        <Td>Jason Phillips</Td>
-      </Tr>
-      <Tr>
-        <Td>Tuscaloosa D3</Td>
-        <Td>29 June 2019</Td>
-        <Td>Github</Td>
-        <Td>The Contributors Consortium</Td>
-        <Td>Data Viz</Td>
-        <Td>Coston Perkins</Td>
-      </Tr>
-    </Tbody>
-  </Table>
-)
-
-`.trim();
+const tableCode = `
+<Table>
+  <Thead>
+    <Tr>
+      <Th>Event</Th>
+      <Th>Date</Th>
+      <Th>Location</Th>
+      <Th>Organizer</Th>
+      <Th>Theme</Th>
+      <Th>Agent</Th>
+    </Tr>
+  </Thead>
+  <Tbody>
+    <Tr>
+      <Td>Tablescon</Td>
+      <Td>9 April 2019</Td>
+      <Td>East Annex</Td>
+      <Td>Super Friends</Td>
+      <Td>Data Tables</Td>
+      <Td>Coston Perkins</Td>
+    </Tr>
+    <Tr>
+      <Td>Capstone Data</Td>
+      <Td>19 May 2019</Td>
+      <Td>205 Gorgas</Td>
+      <Td>Data Underground</Td>
+      <Td>Data Scence</Td>
+      <Td>Jason Phillips</Td>
+    </Tr>
+    <Tr>
+      <Td>Tuscaloosa D3</Td>
+      <Td>29 June 2019</Td>
+      <Td>Github</Td>
+      <Td>The Contributors Consortium</Td>
+      <Td>Data Viz</Td>
+      <Td>Coston Perkins</Td>
+    </Tr>
+  </Tbody>
+</Table>
+  `.trim();
