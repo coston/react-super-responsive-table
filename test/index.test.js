722f621002b83ac5ac802022583cdd1943cdfe5f
Coston Perkins <costonperkins@me.com>
2023-09-01 13:58:33 -0500

    test: refactor extend expect

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
 
 import { Table, Thead, Tbody, Tr, Th, Td } from '../src/index';
 
