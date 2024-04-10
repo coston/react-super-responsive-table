2073625ba2ba698c67544d02e6bf70218a741883
Coston Perkins <costonperkins@me.com>
2024-04-10 09:13:34 -0500

    ci: run lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,6 @@ module.exports = {
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     'react/jsx-props-no-spreading': 0,
+    'react/require-default-props': 0,
   },
 };
