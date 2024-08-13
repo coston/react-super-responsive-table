6b2322aeff562e6a06d988d2ef9b2e516729b623
Coston Perkins <costonperkins@me.com>
2024-08-13 16:08:03 -0500

    chore: publish css directly

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../dist/SuperResponsiveTableStyle.css';
+import '../src/SuperResponsiveTableStyle.css';
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
